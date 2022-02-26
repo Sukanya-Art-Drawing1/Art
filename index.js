b3bcb2164cab33c5b31bf1b1c53a484bb3ca025c
Sukanya-Art-Drawing1 <100463306+Sukanya-Art-Drawing1@users.noreply.github.com>
2022-02-26 23:51:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const a =()=>{
+   // alert()
+   const nava =document.getElementById("nava")
+   const nameid =document.getElementById("nameid")
+   const closes =document.getElementById("closes")
+   nava.style.display= "block";
+   nameid.style.display="none";
+   closes.style.display="none";
+}
+
+const b=()=>{
+   const nava =document.getElementById("nava")
+   nava.style.display= "none";
+}
